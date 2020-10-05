048af9759a4c1facf9b461aca9f7ac6bd6b0836d
Ahmed <aahmed25@uncc.edu>
2020-10-04 20:18:30 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,22 +4,39 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-]
-};
+const budget = require ('./budget.json');
+// {
+//     myBudget: [
+//     {
+//         title: 'Eat out',
+//         budget: 70
+//     },
+//     {
+//         title: 'Rent',
+//         budget: 800
+//     },
+//     {
+//         title: 'Grocery',
+//         budget: 110
+//     },
+//     {
+//         title: 'Phone',
+//         budget: 80
+//     },
+//     {
+//         title: 'Insurance',
+//         budget: 140
+//     },
+//     {
+//         title: 'Car',
+//         budget: 300
+//     },
+//     {
+//         title: 'Bills',
+//         budget: 400
+//     },
+// ]
+// };
 
 
 app.get('/hello', (req, res) => {
